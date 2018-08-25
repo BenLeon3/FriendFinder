f915972e03f17b09afcd8cc490c10508cfbb0e7f
Benjamin Leon <benleon3@gmail.com>
2018-08-25 09:49:05 -0700

    working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,7 +18,88 @@ var friendArray = [
         4,
         1
         ]
+    },
+    {
+    "name":"Juan Hierbas",
+    "photo":"https://randomuser.me/api/portraits/lego/6.jpg",
+    "scores":[
+        5,
+        3,
+        4,
+        1,
+        4,
+        1,
+        2,
+        1,
+        1,
+        1
+        ]
+    },
+    {
+    "name":"El Bandito",
+    "photo":"https://randomuser.me/api/portraits/lego/6.jpg",
+    "scores":[
+        1,
+        1,
+        5,
+        5,
+        5,
+        2,
+        2,
+        5,
+        1,
+        1
+        ]
+    },
+    {
+    "name":"Juan Chido",
+    "photo":"https://randomuser.me/api/portraits/lego/6.jpg",
+    "scores":[
+        1,
+        1,
+        5,
+        5,
+        5,
+        1,
+        1,
+        5,
+        1,
+        1
+        ]
+    },
+    {
+    "name":"Pepito",
+    "photo":"https://randomuser.me/api/portraits/lego/6.jpg",
+    "scores":[
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+        ]
+    },
+    {
+    "name":"Cepillin",
+    "photo":"https://randomuser.me/api/portraits/lego/6.jpg",
+    "scores":[
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+        ]
     }
+
 ];
 
-module.exports = friendArray;
+module.exports = friendArray
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,53 @@
-// ===============================================================================
-// LOAD DATA
-// ===============================================================================
+// // ===============================================================================
+// // LOAD DATA
+// // ===============================================================================
+var friends = require('../data/friends.js');
+// // var Scores = friendData[1].scores;
+// // console.log(Scores);
+// // ===============================================================================
+// // ROUTING
+// // ===============================================================================
 
-var friendData = require("../data/friends");
+module.exports = function(app) {
+	// API GET Requests
+	app.get('/api/friends', function(req, res) {
+		res.json(friends);
+	});
+    // API POST Requests
+	app.post('/api/friends', function(req, res) {
+		
+		var newFriend = req.body;
+		var userResponses = newFriend.scores;
+		console.log('userResponses = ' + userResponses);
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 10000;
 
-module.exports = function(app){
-    app.get("/api/friends", function(req, res) {
-        res.json(friendData);
-    });
+		
+		for (var i = 0; i < friends.length; i++) {
 
-    app.post("/api/friends", function(req, res) {
-        friendData.push(req.body);
-        res.json(friendData);
-    });
+            var diff = 0;
+            
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+			console.log('diff = ' + diff);
+
+			if (diff < totalDifference) {
+				console.log('Closest match found = ' + diff);
+				console.log('Friend name = ' + friends[i].name);
+				console.log('Friend image = ' + friends[i].photo);
+
+				totalDifference = diff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
+			}
+		}
+
+		friends.push(newFriend);
+
+		// Send appropriate response
+		res.json({status: 'OK', name: matchName, photo: matchImage});
+	});
 };
